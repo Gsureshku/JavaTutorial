ccf1f4344f1d21c0a15a94550a4ca0506a81e7e7
Gayathri Sureshkumar <gsureshku@gmail.com>
2018-04-18 17:48:15 -0400

    commit Demo.java

---
+++ Demo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Demo {
+
+  public static void main(String[] args) {
+
+  Scanner scan = new Scanner(System.in);
+  System.out.println("Enter you number:");
+
+  int num = scan.nextInt();
+
+  scan.close();
+
+  System.out.println("The number entered by user: " + num);
+}
+}
